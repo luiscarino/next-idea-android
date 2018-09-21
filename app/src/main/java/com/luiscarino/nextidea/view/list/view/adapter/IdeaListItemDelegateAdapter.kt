f9cd6704798558473856e6f0ba426d2a93837f10
Luis Carino <luiscarinodev@gmail.com>
2018-09-20 21:54:15 -0400

    Using kotlin view binding in delegate adapter. Fixed rate me icon

--- app/src/main/java/com/luiscarino/nextidea/view/list/view/adapter/IdeaListItemDelegateAdapter.kt
+++ app/src/main/java/com/luiscarino/nextidea/view/list/view/adapter/IdeaListItemDelegateAdapter.kt
@@ -4,14 +4,16 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.luiscarino.nextidea.R
 import com.luiscarino.nextidea.util.ConstantsDelegateAdapter
 import com.luiscarino.nextidea.util.recyclerview.DelegateAdapter
 import com.luiscarino.nextidea.util.toButtonColor
 import com.luiscarino.nextidea.util.toDrawableId
+import kotlinx.android.synthetic.main.recyclerview_idea_item.view.*
 
+/**
+ * Encapsulates how to display a list item.
+ */
 class IdeaListItemDelegateAdapter(val actions: Actions) : DelegateAdapter
 <IdeaListItemDelegateAdapter.IdeaItemViewHolder, IdeaListItemRecyclerRecyclerViewType> {
 
@@ -31,22 +33,21 @@ class IdeaListItemDelegateAdapter(val actions: Actions) : DelegateAdapter
 
     class IdeaItemViewHolder(itemView: View, val actions: Actions) : RecyclerView.ViewHolder(itemView) {
 
-        private var titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
-        private var iconImageView: ImageView = itemView.findViewById(R.id.iconImageView)
-        private var categoryNameTextView: TextView = itemView.findViewById(R.id.categoryName)
-        private var lastEditTextView: TextView = itemView.findViewById(R.id.lastEditTextView)
-        private var statusTextView: TextView = itemView.findViewById(R.id.statusTextView)
         private var formatterLastEdit = itemView.context.getString(R.string.formatter_item_last_edit)
 
         fun onBind(item: IdeaListItemRecyclerRecyclerViewType?) {
-            titleTextView.text = item?.title
-            val categoryName = item?.categoryName
-            iconImageView.setImageDrawable(iconImageView.context.getDrawable(toDrawableId(categoryName)))
-            categoryNameTextView.text = categoryName
-            lastEditTextView.text = String.format(formatterLastEdit, item?.lastEdited)
+            itemView.titleTextView.text = item?.title
+
+            itemView.iconImageView.setImageDrawable(itemView.context.getDrawable(toDrawableId(item?.categoryName)))
+
+            itemView.categoryName.text = item?.categoryName
+
+            itemView.lastEditTextView.text = String.format(formatterLastEdit, item?.lastEdited)
+
             val statusName = item?.statusName
-            statusTextView.text = statusName
-            statusTextView.setBackgroundColor(statusTextView.resources.getColor(toButtonColor(statusName)))
+            itemView.statusTextView.text = statusName
+            itemView.statusTextView.setBackgroundColor(itemView.resources.getColor(toButtonColor(statusName)))
+
             itemView.setOnClickListener { actions.onCardClicked(item?.id) }
         }
 
